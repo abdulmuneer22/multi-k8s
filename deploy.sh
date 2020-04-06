fb2b9677ee852daa364ed7d51e16bf32af0bbe08
Muneer PP <abdulmuneer22@gmail.com>
2020-04-06 15:45:22 +0530

    travis config added

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t nageshvashist/multi-client:latest -t nageshvashist/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t nageshvashist/multi-server:latest -t nageshvashist/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t nageshvashist/multi-worker:latest -t nageshvashist/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push nageshvashist/multi-client:latest
-docker push nageshvashist/multi-server:latest
-docker push nageshvashist/multi-worker:latest
-docker push nageshvashist/multi-client:$SHA
-docker push nageshvashist/multi-server:$SHA
-docker push nageshvashist/multi-worker:$SHA
+docker build -t skitecode/multi-client:latest -t skitecode/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t skitecode/multi-server:latest -t skitecode/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t skitecode/multi-worker:latest -t skitecode/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push skitecode/multi-client:latest
+docker push skitecode/multi-server:latest
+docker push skitecode/multi-worker:latest
+docker push skitecode/multi-client:$SHA
+docker push skitecode/multi-server:$SHA
+docker push skitecode/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=nageshvashist/multi-client:$SHA
-kubectl set image deployments/server-deployment server=nageshvashist/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=nageshvashist/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=skitecode/multi-client:$SHA
+kubectl set image deployments/server-deployment server=skitecode/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=skitecode/multi-worker:$SHA
